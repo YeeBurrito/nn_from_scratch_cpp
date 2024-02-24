088b2a581f5f60d0b5d97f51e7ba3477717bfd95
Ethan <81653895+YeeBurrito@users.noreply.github.com>
2024-02-24 01:50:54 -0800

    Fixed class constructor syntax, added weights and bias initialization, added forward prop function

--- neuralnet.cpp
+++ neuralnet.cpp
@@ -2,123 +2,252 @@
 #include <vector>
 #include <cstring>
 #include <algorithm>
+#include <numeric>
 #include "neuralnet.h"
 
 using std::vector;
 
-class neuralnetclass : public neuralnet
+// Class constructor
+neuralnet::neuralnet(int input_size, int nlayers, int output_size, char *hidden_act_fun, double init_range, int nunits, double learn_rate, int mb_size, char type)
 {
-private:
-    char *hidden_act_fun = "sig";
+    this->input_size = input_size;
+    this->nlayers = nlayers;
+    this->output_size = output_size;
+    this->hidden_act_fun = hidden_act_fun;
+    this->init_range = init_range;
+    this->nunits = nunits;
+    this->learn_rate = learn_rate;
+    this->mb_size = mb_size;
+    this->type = type;
 
-public:
-    vector<double> hidden_act(vector<double> list)
+    // Initialize weights and biases
+    for (int i = 0; i < nlayers + 1; i++)
     {
-        int size = list.size();
-        vector<double> result(size);
-
-        if (strcmp(hidden_act_fun, "sig") == 0)
+        int wb_input_size = 0;
+        int wb_output_size = 0;
+        // Get the correct input and output size for the first and last layers
+        if (i == 0)
         {
-            for (int i = 0; i < size; i++)
-            {
-                result[i] = 1 / (1 + exp(-list[i]));
-            }
-        }
-        else if (strcmp(hidden_act_fun, "tanh") == 0)
-        {
-            for (int i = 0; i < size; i++)
-            {
-                result[i] = tanh(list[i]);
-            }
-        }
-        else if (strcmp(hidden_act_fun, "relu") == 0)
-        {
-            for (int i = 0; i < size; i++)
-            {
-                result[i] = list[i] > 0 ? list[i] : 0;
-            }
+            wb_input_size = input_size;
         }
         else
         {
-            std::cerr << "Error: Invalid hidden activation function\n";
-            exit(1);
+            wb_input_size = nunits;
         }
-
-        return result;
-    }
-
-    vector<double> deriv_hidden_act(vector<double> list)
-    {
-        int size = list.size();
-        vector<double> result(size);
-
-        if (strcmp(hidden_act_fun, "sig") == 0)
+        if (i == nlayers)
         {
-            vector<double> sig = hidden_act(list);
-            for (int i = 0; i < size; i++)
-            {
-                result[i] = sig[i] * (1 - sig[i]);
-            }
-        }
-        if (strcmp(hidden_act_fun, "tanh") == 0)
-        {
-            for (int i = 0; i < size; i++)
-            {
-                vector<double> tanh = hidden_act(list);
-                result[i] = 1 - tanh[i] * tanh[i];
-            }
-        }
-        else if (strcmp(hidden_act_fun, "relu") == 0)
-        {
-            for (int i = 0; i < size; i++)
-            {
-                result[i] = list[i] >= 0 ? 1 : 0;
-            }
+            wb_output_size = output_size;
         }
         else
         {
-            std::cerr << "Error: Invalid hidden activation function\n";
-            exit(1);
+            wb_output_size = nunits;
         }
 
-        return result;
-    }
+        vector<vector<double>> weights_layer(wb_input_size, vector<double>(wb_output_size));
+        vector<double> biases_layer(wb_output_size);
 
-    vector<double> softmax(vector<double> list)
+        for (int j = 0; j < wb_input_size; j++)
+        {
+            for (int k = 0; k < wb_output_size; k++)
+            {
+                weights_layer[j][k] = (double)rand() / RAND_MAX * init_range * 2 - init_range;
+            }
+
+            biases_layer[j] = (double)rand() / RAND_MAX * init_range * 2 - init_range;
+        }
+
+        weights.push_back(weights_layer);
+        biases.push_back(biases_layer);
+    }
+}
+
+vector<double> neuralnet::hidden_act(vector<double> list)
+{
+    int size = list.size();
+    vector<double> result(size);
+
+    if (strcmp(hidden_act_fun, "sig") == 0)
     {
-        int size = list.size();
-        vector<double> result(size);
-        double sum = 0;
-
-        // Find the maximum value in the list
-        double max_val = *max_element(list.begin(), list.end());
-
         for (int i = 0; i < size; i++)
         {
-            // Subtract the maximum value before exponentiating
-            result[i] = exp(list[i] - max_val);
-            sum += result[i];
+            result[i] = 1 / (1 + exp(-list[i]));
         }
-
-        for (int i = 0; i < size; i++)
-        {
-            result[i] /= sum;
-        }
-
-        return result;
     }
-};
+    else if (strcmp(hidden_act_fun, "tanh") == 0)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            result[i] = tanh(list[i]);
+        }
+    }
+    else if (strcmp(hidden_act_fun, "relu") == 0)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            result[i] = list[i] > 0 ? list[i] : 0;
+        }
+    }
+    else
+    {
+        std::cerr << "Error: Invalid hidden activation function\n";
+        exit(1);
+    }
+
+    return result;
+}
+
+vector<double> neuralnet::deriv_hidden_act(vector<double> list)
+{
+    int size = list.size();
+    vector<double> result(size);
+
+    if (strcmp(hidden_act_fun, "sig") == 0)
+    {
+        vector<double> sig = hidden_act(list);
+        for (int i = 0; i < size; i++)
+        {
+            result[i] = sig[i] * (1 - sig[i]);
+        }
+    }
+    if (strcmp(hidden_act_fun, "tanh") == 0)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            vector<double> tanh = hidden_act(list);
+            result[i] = 1 - tanh[i] * tanh[i];
+        }
+    }
+    else if (strcmp(hidden_act_fun, "relu") == 0)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            result[i] = list[i] >= 0 ? 1 : 0;
+        }
+    }
+    else
+    {
+        std::cerr << "Error: Invalid hidden activation function\n";
+        exit(1);
+    }
+
+    return result;
+}
+
+vector<double> neuralnet::softmax(vector<double> list)
+{
+    int size = list.size();
+    vector<double> result(size);
+    double sum = 0;
+
+    // Find the maximum value in the list
+    double max_val = *max_element(list.begin(), list.end());
+
+    for (int i = 0; i < size; i++)
+    {
+        // Subtract the maximum value before exponentiating
+        result[i] = exp(list[i] - max_val);
+        sum += result[i];
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] /= sum;
+    }
+
+    return result;
+}
+
+forward_pass_result neuralnet::forward(vector<double> inputs)
+{
+    // Store the preactivations and activations for each layer
+    vector<vector<double>> preactivations;
+    vector<vector<double>> activations;
+    vector<double> layer_input = inputs;
+
+    // Calculate the preactivations and activations for all of the hidden layers
+    for (int i = 0; i < nlayers; i++)
+    {
+        vector<vector<double>> layer_weights = weights[i];
+        vector<double> layer_biases = biases[i];
+        vector<double> layer_preactivation(layer_weights.size());
+
+        for (int j = 0; j < layer_weights.size(); j++)
+        {
+            double preactivation = 0;
+            for (int k = 0; k < layer_weights[j].size(); k++)
+            {
+                preactivation += layer_weights[j][k] * layer_input[k];
+            }
+            preactivation += layer_biases[j];
+            layer_preactivation[j] = preactivation;
+        }
+
+        preactivations.push_back(layer_preactivation);
+
+        vector<double> layer_activation = hidden_act(layer_preactivation);
+        activations.push_back(layer_activation);
+
+        layer_input = layer_activation;
+    }
+
+    // Calculate the preactivations and activations for the output layer
+    vector<vector<double>> output_weights = weights[nlayers];
+    vector<double> output_biases = biases[nlayers];
+
+    vector<double> output_preactivation(output_weights.size());
+    for (int i = 0; i < output_weights.size(); i++)
+    {
+        double preactivation = 0;
+        for (int j = 0; j < output_weights[i].size(); j++)
+        {
+            preactivation += output_weights[i][j] * layer_input[j];
+        }
+        preactivation += output_biases[i];
+        output_preactivation[i] = preactivation;
+    }
+
+    preactivations.push_back(output_preactivation);
+
+    // If the problem is classification, use the softmax function
+    vector<double> output_activation;
+    if (type == 'c')
+    {
+        output_activation = softmax(output_preactivation);
+    }
+    else
+    {
+        output_activation = output_preactivation;
+    }
+
+    activations.push_back(output_activation);
+
+    forward_pass_result result;
+    result.activations = activations;
+    result.preactivations = preactivations;
+
+    return result;
+}
+
+vector<vector<double>> transpose_2d_vector(vector<vector<double>> v)
+{
+    int n = v.size();
+    int m = v[0].size();
+    vector<vector<double>> result(m, vector<double>(n));
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            result[j][i] = v[i][j];
+        }
+    }
+
+    return result;
+}
 
 int main()
 {
-    // test softmax
-    vector<double> list = {3.2, 1.3, 0.2, 747.0};
-
-    neuralnetclass nn;
-    vector<double> result = nn.softmax(list);
-
-    for (int i = 0; i < result.size(); i++)
-    {
-        std::cout << result[i] << " ";
-    }
+    // test weight initialization
+    // neuralnet(int input_size, int nlayers, int output_size, char *hidden_act_fun, double init_range, int nunits, double learn_rate, int mb_size, char type)
+    neuralnet *nn = new neuralnet(5, 2, 2, "sig", 0.5, 3, 0.1, 32, 'c');
 }
--- neuralnet.h
+++ neuralnet.h
@@ -1,12 +1,36 @@
+#pragma once
+
 #include <vector>
+
+struct forward_pass_result
+{
+    std::vector<std::vector<double>> activations;
+    std::vector<std::vector<double>> preactivations;
+};
 
 class neuralnet
 {
 private:
-    char *hidden_act_fun = "sig";
+    int input_size;                                        // Number of neurons in the input layer
+    int nlayers;                                           // Number of hidden layers
+    int output_size;                                       // Number of neurons in the output layer, also the number of classes
+    char *hidden_act_fun;                                  // Hidden layer activation function
+    double init_range;                                     // Range for the initial weights and biases
+    int nunits;                                            // Number of neurons in each hidden layer
+    double learn_rate;                                     // Learning rate
+    int mb_size;                                           // Mini-batch size
+    char type;                                             // Type of neural network, r for regression, c for classification
+    std::vector<std::vector<std::vector<double>>> weights; // Weights for each layer
+    std::vector<std::vector<double>> biases;               // Biases for each layer
 
 public:
+    // Class constructor
+    neuralnet(int input_size, int nlayers, int output_size, char *hidden_act_fun, double init_range, int nunits, double learn_rate, int mb_size, char type);
+
     std::vector<double> hidden_act(std::vector<double> list);
     std::vector<double> deriv_hidden_act(std::vector<double> list);
     std::vector<double> softmax(std::vector<double> list);
+    forward_pass_result forward(std::vector<double> inputs);
 };
+
+std::vector<std::vector<double>> transpose_2d_vector(std::vector<std::vector<double>> v);
